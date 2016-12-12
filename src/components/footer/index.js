d9c76e999a040e61af80295f98218d585fe99ba0
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-12 21:49:05 -0200

    social links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,11 +7,11 @@ class Footer extends Component {
 
 			<footer className="footer">
 	          <ul>
-	            <li><a href="#">twitter</a></li>
-	            <li><a href="#">facebook</a></li>
-	            <li><a href="#">linkedin</a></li>
-	            <li><a href="#">instagram</a></li>
-	            <li><a href="#">e-mail</a></li>
+	            <li><a href="http://twitter.com/rafaellucas" target="_blank">twitter</a></li>
+	            <li><a href="http://facebook.com.br/rafaellucass" target="_blank">facebook</a></li>
+	            <li><a href="http://linkedin.com/in/rafaellucass" target="_blank">linkedin</a></li>
+	            <li><a href="https://www.instagram.com/rafaellucass/" target="_blank">instagram</a></li>
+	            <li><a href="mailto:contato@rafaellucas.com.br" target="_blank">e-mail</a></li>
 	          </ul>
 	        </footer>
 
