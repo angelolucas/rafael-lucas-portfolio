eea81d0c9325ea7f2766c7f53ce2b75d51cbf87d
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-14 00:20:52 -0200

    gsap

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import TweenMax from 'gsap';
 import './index.css'
 
 class HeroIntro extends Component {
+	componentDidMount() {
+		const title = ReactDOM.findDOMNode(this.refs.title);
+		
+		TweenMax.from (
+			title,
+			2, 
+			{
+				opacity: 0,
+				x: 100
+			}
+		)
+	}
 	render() {
 		return (
 
 			<header className="hero-intro">
-				<h1 className="hero-intro__title">Rafael Lucas</h1>
+				<h1 ref="title" className="hero-intro__title">Rafael Lucas</h1>
 				<h2 className="hero-intro__description">I am a brazilian designer specializing in interactive experiences living in Brasília, Brazil.</h2>
 
 				<nav className="hero-intro__anchoring">
