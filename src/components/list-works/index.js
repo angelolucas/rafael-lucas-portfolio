3fe1cdd6f8bef8846a12543768131ac0889c0bc5
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-16 19:35:12 -0200

    thumbs

--- src/components/list-works/index.js
+++ src/components/list-works/index.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import './index.css';
-import thumb from './thumb.jpg';
-import thumbOutside from './thumb-outside.png';
+import movamaisthumb from '../../containers/mova-mais/thumb.png';
+import movamaisthumboverlay from '../../containers/mova-mais/thumb-overlay.png';
+
+import caixanasruasthumb from '../../containers/caixa-nas-ruas/thumb.png';
+import caixanasruasthumboverlay from '../../containers/caixa-nas-ruas/thumb-overlay.png';
+
+import bbprivetethumb from '../../containers/bb-privete/thumb.png';
+
+import davidyurmanthumb from '../../containers/david-yurman/thumb.png';
+
+import encinterthumb from '../../containers/encinter/thumb.png';
+import encinterthumboverlay from '../../containers/encinter/thumb-overlay.png';
+
+import emicidathumb from '../../containers/emicida/thumb.png';
+import emicidathumboverlay from '../../containers/emicida/thumb-overlay.png';
 
 class ListWorks extends Component {
   
   render() {
-    const thumbOutsideBg = {
-      backgroundImage: 'url(' + thumbOutside + ')'
+    const movamaisthumboverlayBg = {
+      backgroundImage: 'url(' + movamaisthumboverlay + ')'
+    };
+    const caixanasruasthumboverlayBg = {
+      backgroundImage: 'url(' + caixanasruasthumboverlay + ')'
+    };
+    const encinterthumboverlayBg = {
+      backgroundImage: 'url(' + encinterthumboverlay + ')'
+    };
+    const emicidathumboverlayBg = {
+      backgroundImage: 'url(' + emicidathumboverlay + ')'
     };
 
     return (
       <ul className="list-works">
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={movamaisthumb} alt="Mova mais" />
+            <div style={movamaisthumboverlayBg}></div>
           </div>
           <h3 className="list-works__title">Mova Mais - Startup</h3>
           <p className="list-works__category">app</p>
@@ -25,8 +47,8 @@ class ListWorks extends Component {
         </li>
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={caixanasruasthumb} alt="Caixa nas Ruas" />
+            <div style={caixanasruasthumboverlayBg}></div>
           </div>
           <h3 className="list-works__title">Caixa nas Ruas</h3>
           <p className="list-works__category">web desktop & mobile</p>
@@ -34,8 +56,7 @@ class ListWorks extends Component {
         </li>
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={bbprivetethumb} alt="BB Privete" />
           </div>
           <h3 className="list-works__title">BB Privete</h3>
           <p className="list-works__category">web desktop</p>
@@ -43,8 +64,7 @@ class ListWorks extends Component {
         </li>
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={davidyurmanthumb} alt="David Yurman" />
           </div>
           <h3 className="list-works__title">David Yurman</h3>
           <p className="list-works__category">web mobile</p>
@@ -52,8 +72,8 @@ class ListWorks extends Component {
         </li>
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={encinterthumb} alt="Encinter" />
+            <div style={encinterthumboverlayBg}></div>
           </div>
           <h3 className="list-works__title">Encinter</h3>
           <p className="list-works__category">web mobile</p>
@@ -61,8 +81,8 @@ class ListWorks extends Component {
         </li>
         <li>
           <div className="list-works__thumb">
-            <img src={thumb} alt="Mova mais" />
-            <div style={thumbOutsideBg}></div>
+            <img src={emicidathumb} alt="Emicida" />
+            <div style={emicidathumboverlayBg}></div>
           </div>
           <h3 className="list-works__title">Emicida</h3>
           <p className="list-works__category">web mobile</p>
