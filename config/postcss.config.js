e743724c0e2823a58a3b1581ba7c4c6e2f03c375
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-15 23:10:44 -0200

    fixed footer

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,8 @@
 // Export the PostCSS Config for usage in webpack
 module.exports = {
 	plugins: [
+		require("postcss-import"),
+		 
 		/* Use tomorrow’s CSS syntax, today. */
 		require('postcss-cssnext'),
 
