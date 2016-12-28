24d8749a99d464ca2186d65aa0c7cb03d7d354d3
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-27 23:52:38 -0200

    mova mais - intro

--- src/containers/mova-mais/index.js
+++ src/containers/mova-mais/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import mockupTop from './images/mockup-top.png';
+
 class MovaMais extends Component {
   componentDidMount() {
     document.body.classList.add('single-work-active');
@@ -9,22 +11,18 @@ class MovaMais extends Component {
   }
   render() {
     return (
-      <section className="mova-mais">
-        <div className="mova-mais__hero" />
-        <h2 className="mova-mais__title">Mova Mais</h2>
-        <p>lorem ipsum</p>
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random</p>
-        <p>text. It has roots in a piece of classical Latin literature fro</p>
-        <p>BC, making it over 2000 years old. Richard McClintock, a Latin </p>
-        <p>professor at Hampden-Sydney College in Virginia, looked up one </p>
-        <p>the more obscure Latin words, consectetur, from a Lorem Ipsum </p>
-        <p>passage, and going through the cites of the word in classical </p>
-        <p>literature, discovered the undoubtable source. Lorem Ipsum come</p>
-        <p>from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et </p>
-        <p>The Extremes of Good and Evil) by Cicero, written in 45 BC. </p>
-        <p>book is a reatise on the theory of ethics, very popular during </p>
-        <p>Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor </p>
-        <p>amet.., comes from a line in section 1.10.32.</p>
+      <section className="mm">
+        <div className="mm__hero" />
+        <div className="mm__intro">
+          <div className="container">
+            <h1 className="mm__intro__title">Mova Mais</h1>
+            <h2 className="mm__intro__concept-h">Concept</h2>
+            <h3 className="mm__intro__concept">Ganhe pontos por fazer exercícios.</h3>
+          </div>
+        </div>
+        <div className="container">
+          <img src={mockupTop} alt="" />
+        </div>
       </section>
     );
   }
