aaf53a135a6e2c0c9b002efa61386c3c8268d074
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-28 23:47:00 -0200

    mova mais - flow

--- src/containers/mova-mais/index.js
+++ src/containers/mova-mais/index.js
@@ -29,6 +29,22 @@ class MovaMais extends Component {
             <p>{'Apresentar claramente a proposta de valor, tendo sempre um crescimento consistente da base de usuários, atrelado a uma interface amigável, concisa e focada no usuário.'}</p>
           </div>
         </div>
+        <div className="mm__step mm__step0">
+          <div className="container">
+            <h3 className="mm__step__title"><span>01</span> flow</h3>
+            <ul className="mm__step0__items">
+              <li>
+                <p>Monitor<br /> physical activities</p>
+              </li>
+              <li>
+                <p>Reach the<br /> daily goal</p>
+              </li>
+              <li>
+                <p>Join points<br /> to convert</p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
     );
   }
