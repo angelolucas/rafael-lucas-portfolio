01491d320507851b8b7c2b45166128c8cd4cbdd8
Ângelo Lucas <angelo.lrc@gmail.com>
2016-12-29 22:33:54 -0200

    mova mais - step04

--- src/containers/mova-mais/index.js
+++ src/containers/mova-mais/index.js
@@ -4,6 +4,9 @@ import mockupTop from './images/mockup-top.jpg';
 import step01 from './images/step1.png';
 import step02 from './images/step2.png';
 import step03 from './images/step3.png';
+import engagement01 from './images/engagement01.png';
+import engagement02 from './images/engagement02.png';
+import engagement03 from './images/engagement03.png';
 
 
 class MovaMais extends Component {
@@ -16,7 +19,11 @@ class MovaMais extends Component {
   render() {
     return (
       <section className="mm">
+
+        {/* Hero Image */}
         <div className="mm__hero" />
+
+        {/* Intro */}
         <div className="mm__intro">
           <div className="container">
             <h1 className="mm__intro__title">Mova Mais</h1>
@@ -27,12 +34,16 @@ class MovaMais extends Component {
         <div className="container">
           <img src={mockupTop} alt="" />
         </div>
+
+        {/* Objective */}
         <div className="mm__objective">
           <div className="container">
             <h2>Objective</h2>
             <p>{'Apresentar claramente a proposta de valor, tendo sempre um crescimento consistente da base de usuários, atrelado a uma interface amigável, concisa e focada no usuário.'}</p>
           </div>
         </div>
+
+        {/* Step 01 */}
         <div className="mm__step mm__step01">
           <div className="container">
             <h3 className="mm__step__title"><span>01</span> Flow</h3>
@@ -49,6 +60,8 @@ class MovaMais extends Component {
             </ul>
           </div>
         </div>
+
+        {/* Step 02 */}
         <div className="mm__step mm__step02">
           <div className="container">
             <h3 className="mm__step__title"><span>02</span> Walkthroughs</h3>
@@ -81,12 +94,51 @@ class MovaMais extends Component {
             </ul>
           </div>
         </div>
+
+        {/* Step 03 */}
         <div className="mm__step mm__step03">
           <div className="container">
             <h3 className="mm__step__title"><span>03</span> Daily Goal</h3>
             <p className="mm__step__description">{'Um dos maiores objetivos da aplicativo é fazer com que o usuário se sinta sempre motivado a se exercitar, por isso exibimos mensagens de incentivo, sobretudo para não perder a sequência de exercícios.'}</p>
           </div>
           <div className="mm__step03__background" />
+        </div>
+
+        {/* Step 04 */}
+        <div className="mm__step mm__step04">
+          <div className="container">
+            <h3 className="mm__step__title"><span>04</span> Engagement</h3>
+            <p className="mm__step__description">{'Reter o usuário é sempre um grande desafio se tratando de atividade física, por isso elaboramos um recurso chamado “Combo”, que beneficia com mais pontos os usuários que atiguem a meta diária com mais frequência.'}</p>
+            <ul className="mm__chess-list">
+              <li>
+                <div className="image">
+                  <img src={engagement01} alt="" />
+                </div>
+                <div className="text">
+                  <h4>Hit Target!</h4>
+                  <p>{'“Você ganhou 20 pontos por cumprir sua meta pela primeira vez!”'}</p>
+                </div>
+              </li>
+              <li>
+                <div className="image">
+                  <img src={engagement02} alt="" />
+                </div>
+                <div className="text">
+                  <h4>Keep Your Combo</h4>
+                  <p>{'“Criando uma sequência de metas atingidas você pode ganhar até 40 pontos por cumprimento de meta.”'}</p>
+                </div>
+              </li>
+              <li>
+                <div className="image">
+                  <img src={engagement03} alt="" />
+                </div>
+                <div className="text">
+                  <h4>Rest Is Important</h4>
+                  <p>{'“Você pode descansar por até 2 dias seguidos sem quebrar seu combo.”'}</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     );
