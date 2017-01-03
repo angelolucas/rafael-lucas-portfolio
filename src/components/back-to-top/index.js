e71d0097e520a4ca89a00046073b3892456c7e08
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-03 15:07:45 -0200

    back to top - hide and show

--- src/components/back-to-top/index.js
+++ src/components/back-to-top/index.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import TweenLite from 'gsap';
 
 class BackToTop extends Component {
+  componentDidMount() {
+    const container = document.querySelector('.single-work .scroll');
+    const height = 1000;
+    const button = this.button;
+
+    container.onscroll = () => {
+      if (container.scrollTop > height) {
+        button.classList.add('back-to-top--show');
+      } else {
+        button.classList.remove('back-to-top--show');
+      }
+    };
+  }
   backToTop() {
     TweenLite.to(
       document.querySelector('.single-work .scroll'),
