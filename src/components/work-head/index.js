f0517330c591024a10330aca8c631da5d9bbca6a
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-03 22:41:42 -0200

    work head - down button

--- src/components/work-head/index.js
+++ src/components/work-head/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import TweenLite from 'gsap';
 
 class WorkHead extends Component {
-  componentDidMount() {
-
+  downButton() {
+    TweenLite.to(
+      document.querySelector('.single-work .scroll'),
+      0.5,
+      {
+        scrollTo: {
+          y: window.innerHeight,
+        },
+      },
+    );
   }
   render() {
     return (
-      <div className="work-head" style={{ backgroundImage: `url(${this.props.image})` }} />
+      <div className="work-head" style={{ backgroundImage: `url(${this.props.image})` }}>
+        <button
+          type="button"
+          className="work-head__button"
+          ref={(button) => { this.button = button; }}
+          onClick={this.downButton}
+        />
+      </div>
     );
   }
 }
