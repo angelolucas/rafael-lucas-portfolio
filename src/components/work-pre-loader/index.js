5f8787228ab9c1cae25293553579d2e3276f4a6f
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-09 09:39:07 -0200

    test - preload video

--- src/components/work-pre-loader/index.js
+++ src/components/work-pre-loader/index.js
@@ -1,14 +1,16 @@
 // Dependencies
 import React from 'react';
 
-import movaMaisHead from '../../containers/mova-mais/images/video-poster.jpg';
-import caixaNasRuasHead from '../../containers/caixa-nas-ruas/images/work-head.jpg';
+import posterMovaMais from '../../containers/mova-mais/images/video-poster.jpg';
+import videoMovaMais from '../../containers/mova-mais/images/video.mp4';
+import posterCaixaNasRuas from '../../containers/caixa-nas-ruas/images/work-head.jpg';
 
 function WorkPreLoader() {
   return (
     <div className="work-pre-loader" style={{ display: 'none' }}>
-      <img src={movaMaisHead} alt="" />
-      <img src={caixaNasRuasHead} alt="" />
+      <img src={posterMovaMais} alt="" />
+      <img src={posterCaixaNasRuas} alt="" />
+      <video src={videoMovaMais} />
     </div>
   );
 }
