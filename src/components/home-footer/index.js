08a0d836f50a1f6e5def36f63fd07fa93a20f9a5
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-12 13:42:55 -0200

    footer blank box

--- src/components/home-footer/index.js
+++ src/components/home-footer/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 function HomeFooter() {
   return (
-    <footer className="home-footer">
-      <ul className="home-footer__networks">
-        <li><a href="http://twitter.com/rafaellucas" target="_blank" rel="noopener noreferrer">twitter</a></li>
-        <li><a href="http://facebook.com.br/rafaellucass" target="_blank" rel="noopener noreferrer">facebook</a></li>
-        <li><a href="http://linkedin.com/in/rafaellucass" target="_blank" rel="noopener noreferrer">linkedin</a></li>
-        <li><a href="https://www.instagram.com/rafaellucass/" target="_blank" rel="noopener noreferrer">instagram</a></li>
-        <li><a href="mailto:contato@rafaellucas.com.br" target="_blank" rel="noopener noreferrer">e-mail</a></li>
-      </ul>
-    </footer>
+    <div>
+      <footer className="home-footer">
+        <ul className="home-footer__networks">
+          <li><a href="http://twitter.com/rafaellucas" target="_blank" rel="noopener noreferrer">twitter</a></li>
+          <li><a href="http://facebook.com.br/rafaellucass" target="_blank" rel="noopener noreferrer">facebook</a></li>
+          <li><a href="http://linkedin.com/in/rafaellucass" target="_blank" rel="noopener noreferrer">linkedin</a></li>
+          <li><a href="https://www.instagram.com/rafaellucass/" target="_blank" rel="noopener noreferrer">instagram</a></li>
+          <li><a href="mailto:contato@rafaellucas.com.br" target="_blank" rel="noopener noreferrer">e-mail</a></li>
+        </ul>
+      </footer>
+      <div className="home-footer__blank" />
+    </div>
   );
 }
 
