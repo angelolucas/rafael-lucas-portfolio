3d388560df316207b5d099a9badcb73fb566672a
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-14 23:17:57 -0200

    encinter - video correction

--- src/sections/encinter/index.js
+++ src/sections/encinter/index.js
@@ -12,7 +12,7 @@ function Section() {
     <section className="dy">
 
       {/* Hero Image */}
-      <WorkHead color="black" src={video} type="video" />
+      <WorkHead color="white" src={video} type="video" />
     </section>
   );
 }
