19a794e5025f6c51c0ce6e992d5b8570b807b8c2
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-17 15:53:57 -0200

    david yurman - credits

--- src/sections/david-yurman/index.js
+++ src/sections/david-yurman/index.js
@@ -16,6 +16,8 @@ import search from './media/search.png';
 import shoppingBag from './media/shopping-bag.png';
 import checkout from './media/checkout.png';
 import findAStore from './media/find-a-store.png';
+import guideline from './media/guideline.png';
+import credits from './media/credits.png';
 
 function Section() {
   return (
@@ -53,83 +55,98 @@ function Section() {
         </div>
       </div>
 
-      {/* Step 02 */}
-      <div className="">
+      {/* Pages */}
+      <div className="container">
+        <ul className="chess-list chess-list--align-top">
+          <li>
+            <div>
+              <img src={homePage} alt="Home Page" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Home Page</h4>
+              <p>{'Slideshow com as principais ofertas. Acesso ao menu de navegação e quantidade de produtos na bolsa no header.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={navigation} alt="Navigation" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Navigation</h4>
+              <p>{'Menu simples, com componentes cumprimidos. É possível efetuar uma busca e acessar as demais áreas do site através dele.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={explore} alt="Explore" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Explore</h4>
+              <p>{'É possível visualizar vários produtos ao mesmo tempo, além de filtrar pro preço, tamanho, cor e categoria.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={productDetail} alt="Procuct Detail" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Procuct Detail</h4>
+              <p>{'Na página do produto é exposta imagens bem destacadas, onde é possível visualizar todos os detalhes da peça. Também é exibido as cores disponívels, dimenções, preços e vários outros detalhes.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={search} alt="Search" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Search</h4>
+              <p>{'Busca por produtos com filtros, permitindo uma pesquisa rápida e efeciente.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={shoppingBag} alt="Shopping Bag" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Shopping Bag</h4>
+              <p>{'Detalhes de todos os produtos adicionados na bolsa. Com navegação intuitiva o usuário consegue facilmente remover itens ou finalizar comprar.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={checkout} alt="Checkout" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Checkout</h4>
+              <p>{'Último passo para compra, página onde o usuário preenche dados pessoais e de pagamento para conclusão da compra.'}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={findAStore} alt="Find A Store" />
+            </div>
+            <div className="chess-list__info">
+              <h4 className="chess-list__title">Find A Store</h4>
+              <p>{'Forma fácil de encontrar uma loja próxima, identificando a localização ou inserindo o código postal.'}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      {/* Guideline */}
+      <div className="guideline">
         <div className="container">
-          <ul className="chess-list chess-list--align-top">
-            <li>
-              <div>
-                <img src={homePage} alt="Home Page" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Home Page</h4>
-                <p>{'Slideshow com as principais ofertas. Acesso ao menu de navegação e quantidade de produtos na bolsa no header.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={navigation} alt="Navigation" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Navigation</h4>
-                <p>{'Menu simples, com componentes cumprimidos. É possível efetuar uma busca e acessar as demais áreas do site através dele.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={explore} alt="Explore" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Explore</h4>
-                <p>{'É possível visualizar vários produtos ao mesmo tempo, além de filtrar pro preço, tamanho, cor e categoria.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={productDetail} alt="Procuct Detail" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Procuct Detail</h4>
-                <p>{'Na página do produto é exposta imagens bem destacadas, onde é possível visualizar todos os detalhes da peça. Também é exibido as cores disponívels, dimenções, preços e vários outros detalhes.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={search} alt="Search" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Search</h4>
-                <p>{'Busca por produtos com filtros, permitindo uma pesquisa rápida e efeciente.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={shoppingBag} alt="Shopping Bag" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Shopping Bag</h4>
-                <p>{'Detalhes de todos os produtos adicionados na bolsa. Com navegação intuitiva o usuário consegue facilmente remover itens ou finalizar comprar.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={checkout} alt="Checkout" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Checkout</h4>
-                <p>{'Último passo para compra, página onde o usuário preenche dados pessoais e de pagamento para conclusão da compra.'}</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={findAStore} alt="Find A Store" />
-              </div>
-              <div className="chess-list__info">
-                <h4 className="chess-list__title">Find A Store</h4>
-                <p>{'Forma fácil de encontrar uma loja próxima, identificando a localização ou inserindo o código postal.'}</p>
-              </div>
-            </li>
-          </ul>
+          <h3 className="guideline__title">Guideline</h3>
+          <img className="guideline__image" src={guideline} alt="Guideline" />
+        </div>
+      </div>
+
+      {/* Credits */}
+      <div className="credits">
+        <div className="container">
+          <h3 className="credits__title">Credits</h3>
+          <p className="credits__info">{'Tive como insumo o layout para desktop desenhado pelo então designer da David Yurman, Sung Hean e pelo designer Alex Lins. Minha função no projeto foi desenhar o site para dispositivos móveis juntamente com o designer Adriel Nunes.'}</p>
+          <img src={credits} alt="" />
         </div>
       </div>
 
