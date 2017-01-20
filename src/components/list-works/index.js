53c2310ceef51582c02c2140e8028e05c85d170b
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-19 22:59:32 -0200

    list works - refactoring

--- src/components/list-works/index.js
+++ src/components/list-works/index.js
@@ -1,77 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import movamaisthumb from '../../sections/mova-mais/media/thumb.png';
-import movamaisthumboverlay from '../../sections/mova-mais/media/thumb-overlay.png';
+// Mova mais
+import mmImage from '../../sections/mova-mais/media/thumb.png';
+import mmDetail from '../../sections/mova-mais/media/thumb-overlay.png';
 
-import caixanasruasthumb from '../../sections/caixa-nas-ruas/media/thumb.png';
-import caixanasruasthumboverlay from '../../sections/caixa-nas-ruas/media/thumb-overlay.png';
+// Caixa nas Ruas
+import cnrImage from '../../sections/caixa-nas-ruas/media/thumb.png';
+import cnrDetail from '../../sections/caixa-nas-ruas/media/thumb-overlay.png';
 
-import bbprivetethumb from '../../sections/bb-privete/media/thumb.png';
+// BB Privete
+import bbpImage from '../../sections/bb-privete/media/thumb.png';
 
-import davidyurmanthumb from '../../sections/david-yurman/media/thumb.png';
+// David Yurman
+import dyImage from '../../sections/david-yurman/media/thumb.png';
 
-import encinterthumb from '../../sections/encinter/media/thumb.png';
-import encinterthumboverlay from '../../sections/encinter/media/thumb-overlay.png';
+// Encinter
+import ectImage from '../../sections/encinter/media/thumb.png';
+import ectDetail from '../../sections/encinter/media/thumb-overlay.png';
 
-import emicidathumb from '../../sections/emicida/media/thumb.png';
-import emicidathumboverlay from '../../sections/emicida/media/thumb-overlay.png';
+// Emicida
+import mcdImage from '../../sections/emicida/media/thumb.png';
+import mcdDetail from '../../sections/emicida/media/thumb-overlay.png';
+
+const works = {
+  1: {
+    title: 'Mova Mais',
+    category: 'app',
+    link: '/mova-mais',
+    image: mmImage,
+    detail: mmDetail,
+  },
+  2: {
+    title: 'Caixa Nas Ruas',
+    category: 'web desktop & mobile',
+    link: '/mova-mais',
+    image: cnrImage,
+    detail: cnrDetail,
+  },
+  3: {
+    title: 'BB Private',
+    category: 'web desktop',
+    link: '/bb-privete',
+    image: bbpImage,
+  },
+  4: {
+    title: 'David Yurman',
+    category: 'web mobile',
+    link: '/david-yurman',
+    image: dyImage,
+  },
+  5: {
+    title: 'Encinter',
+    category: 'web mobile',
+    link: '/encinter',
+    image: ectImage,
+    detail: ectDetail,
+  },
+  6: {
+    title: 'Emicida',
+    category: 'web mobile',
+    link: '/emicida',
+    image: mcdImage,
+    detail: mcdDetail,
+  },
+};
 
 function ListWorks() {
   return (
     <ul className="list-works">
-      <li>
-        <div className="list-works__thumb">
-          <img src={movamaisthumb} alt="Mova mais" />
-          <div style={{ backgroundImage: `url(${movamaisthumboverlay})` }} />
-        </div>
-        <h3 className="list-works__title">Mova Mais - Startup</h3>
-        <p className="list-works__category">app</p>
-        <Link className="list-works__link" to="/mova-mais">Mais informações</Link>
-      </li>
-      <li>
-        <div className="list-works__thumb">
-          <img src={caixanasruasthumb} alt="Caixa nas Ruas" />
-          <div style={{ backgroundImage: `url(${caixanasruasthumboverlay})` }} />
-        </div>
-        <h3 className="list-works__title">Caixa nas Ruas</h3>
-        <p className="list-works__category">web desktop & mobile</p>
-        <Link className="list-works__link" to="/caixa-nas-ruas">Mais informações</Link>
-      </li>
-      <li>
-        <div className="list-works__thumb">
-          <img src={bbprivetethumb} alt="BB Privete" />
-        </div>
-        <h3 className="list-works__title">BB Privete</h3>
-        <p className="list-works__category">web desktop</p>
-        <Link className="list-works__link" to="/bb-privete">Mais informações</Link>
-      </li>
-      <li>
-        <div className="list-works__thumb">
-          <img src={davidyurmanthumb} alt="David Yurman" />
-        </div>
-        <h3 className="list-works__title">David Yurman</h3>
-        <p className="list-works__category">web mobile</p>
-        <Link className="list-works__link" to="/david-yurman">Mais informações</Link>
-      </li>
-      <li>
-        <div className="list-works__thumb">
-          <img src={encinterthumb} alt="Encinter" />
-          <div style={{ backgroundImage: `url(${encinterthumboverlay})` }} />
-        </div>
-        <h3 className="list-works__title">Encinter</h3>
-        <p className="list-works__category">web mobile</p>
-        <Link className="list-works__link" to="/encinter">Mais informações</Link>
-      </li>
-      <li>
-        <div className="list-works__thumb">
-          <img src={emicidathumb} alt="Emicida" />
-          <div style={{ backgroundImage: `url(${emicidathumboverlay})` }} />
-        </div>
-        <h3 className="list-works__title">Emicida</h3>
-        <p className="list-works__category">web mobile</p>
-        <Link className="list-works__link" to="/emicida">Mais informações</Link>
-      </li>
+      {
+        Object.keys(works).map((work, key) => (
+          <li className="work-thumbnail" key={key}>
+            <div className="work-thumbnail__window">
+              <img
+                className="work-thumbnail__image"
+                src={works[work].image}
+                alt="thumbnail"
+              />
+              { works[work].detail &&
+              <img
+                className="work-thumbnail__detail"
+                src={works[work].detail}
+                alt=""
+              />
+              }
+            </div>
+            <h3 className="work-thumbnail__title">{works[work].title}</h3>
+            <p className="work-thumbnail__category">{works[work].category}</p>
+            <Link className="work-thumbnail__link" to={works[work].link}>Mais informações</Link>
+          </li>
+        ))
+      }
     </ul>
   );
 }
