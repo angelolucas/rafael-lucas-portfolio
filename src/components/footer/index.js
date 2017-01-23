fc82988e1ba5efe8537fd9fa7cc12fe71a89cbc7
Ângelo Lucas <angelo.lrc@gmail.com>
2017-01-23 00:48:08 -0200

    link underlined componentized

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,19 +7,19 @@ function HomeFooter() {
         <div className="container">
           <ul className="footer__networks">
             <li>
-              <a className="footer__twitter" href="http://twitter.com/rafaellucas" target="_blank" rel="noopener noreferrer">twitter</a>
+              <a className="link-underlined" href="http://twitter.com/rafaellucas" target="_blank" rel="noopener noreferrer">twitter</a>
             </li>
             <li>
-              <a className="footer__facebook" href="http://facebook.com.br/rafaellucass" target="_blank" rel="noopener noreferrer">facebook</a>
+              <a className="link-underlined" href="http://facebook.com.br/rafaellucass" target="_blank" rel="noopener noreferrer">facebook</a>
             </li>
             <li>
-              <a className="footer__linkedin" href="http://linkedin.com/in/rafaellucass" target="_blank" rel="noopener noreferrer">linkedin</a>
+              <a className="link-underlined" href="http://linkedin.com/in/rafaellucass" target="_blank" rel="noopener noreferrer">linkedin</a>
             </li>
             <li>
-              <a className="footer__instagram" href="https://www.instagram.com/rafaellucass/" target="_blank" rel="noopener noreferrer">instagram</a>
+              <a className="link-underlined" href="https://www.instagram.com/rafaellucass/" target="_blank" rel="noopener noreferrer">instagram</a>
             </li>
             <li>
-              <a className="footer__email" href="mailto:contato@rafaellucas.com.br" target="_blank" rel="noopener noreferrer">e-mail</a>
+              <a className="link-underlined" href="mailto:contato@rafaellucas.com.br" target="_blank" rel="noopener noreferrer">e-mail</a>
             </li>
           </ul>
         </div>
