8e4880ff70f31ef86a47572658091905b3497b71
Ângelo Lucas <angelo.lrc@gmail.com>
2017-02-16 23:23:38 -0200

    home effect improvement

--- src/sections/home/index.js
+++ src/sections/home/index.js
@@ -7,14 +7,15 @@ import Footer from '../../components/footer';
 class Home extends Component {
   componentDidMount() {
     const heightEffect = (80 / 100) * window.innerHeight;
+    let opacity = 1;
 
     window.addEventListener('scroll', () => {
-      let opacity = (window.scrollY * 100) / heightEffect; // get scroll in percentage
-      opacity /= 100; // decreases to scale 0 to 1
-      opacity = 1 - opacity; // invert values
-      opacity = opacity.toFixed(3); // limits to 3 decimals
-
-      if (opacity < 0) {
+      if (window.scrollY < heightEffect) {
+        opacity = (window.scrollY * 100) / heightEffect; // get scroll in percentage
+        opacity /= 100; // decreases to scale 0 to 1
+        opacity = 1 - opacity; // invert values
+        opacity = opacity.toFixed(3); // limits to 3 decimals
+      } else {
         opacity = 0;
       }
 
