70e7cf909fab7ab85c8562ac70a067602053f97f
Ângelo Lucas <angelo.lrc@gmail.com>
2017-02-19 20:16:59 -0300

    change email

--- src/sections/contact/index.js
+++ src/sections/contact/index.js
@@ -5,7 +5,7 @@ function Contact() {
     <div className="contact-section">
       <div className="container">
         <h4 className="contact-section__title">email:</h4>
-        <p className="contact-section__value contact-section__value--email">contato@rafaellucas.com.br</p>
+        <p className="contact-section__value contact-section__value--email">bsb.rafael@gmail.com</p>
         <h4 className="contact-section__title">phone:</h4>
         <p className="contact-section__value">+55 61 9 9167 6245</p>
       </div>
