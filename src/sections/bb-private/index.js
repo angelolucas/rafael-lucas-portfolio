6b5ddd6e79e85d22f434c9b375ddc7ed72e7d709
Ângelo Lucas <angelo.lrc@gmail.com>
2017-02-19 14:19:50 -0300

    BB Private - responsive images

--- src/sections/bb-private/index.js
+++ src/sections/bb-private/index.js
@@ -8,12 +8,18 @@ import WorkHead from '../../components/work-head';
 import video from './media/video.mp4';
 import logo from './media/logo.png';
 import mockup from './media/mockup.png';
+import mockupMin from './media/mockup-min.png';
 import siteMap from './media/site-map.png';
 import homePage from './media/home-page.png';
-import privateClient from './media/privat-client.png';
-import investiments from './media/investiments.png';
-import financialAdvisory from './media/financial-advisory.png';
-import aboutTheClient from './media/about-the-client.png';
+import homePageMin from './media/home-page-min.jpg';
+import privateClient from './media/privat-client.jpg';
+import privateClientMin from './media/privat-client-min.jpg';
+import investiments from './media/investiments.jpg';
+import investimentsMin from './media/investiments-min.jpg';
+import financialAdvisory from './media/financial-advisory.jpg';
+import financialAdvisoryMin from './media/financial-advisory-min.jpg';
+import aboutTheClient from './media/about-the-client.jpg';
+import aboutTheClientMin from './media/about-the-client-min.jpg';
 import guideline from './media/guideline.png';
 import mockup2 from './media/mockup2.png';
 
@@ -49,7 +55,10 @@ function Section() {
       {/* Mockup */}
       <div className="work-mockup">
         <div className="container container--full">
-          <img src={mockup} alt="" />
+          <picture>
+            <source srcSet={mockupMin} media="(max-width: 480px)" />
+            <img src={mockup} alt="Mockup" />
+          </picture>
         </div>
       </div>
 
@@ -61,18 +70,26 @@ function Section() {
         </div>
       </div>
 
-      {/* Site Map */}
-      <div className="work-sitemap">
-        <div className="container">
-          <h2>Structure of site</h2>
-          <img src={siteMap} alt="Site Map" />
+      {/*
+        Site Map
+        Disabled for small devices
+      */}
+      { window.innerWidth >= 480 &&
+        <div className="work-sitemap">
+          <div className="container">
+            <h2>Structure of site</h2>
+            <img src={siteMap} alt="Site Map" />
+          </div>
         </div>
-      </div>
+      }
 
       {/* Home Page Screens */}
       <div className="work-screen">
         <div className="container">
-          <img className="work-screen__image" src={homePage} alt="Home Page" />
+          <picture>
+            <source srcSet={homePageMin} media="(max-width: 480px)" />
+            <img className="work-screen__image" src={homePage} alt="Home Page" />
+          </picture>
           <div className="work-screen__info">
             <h3 className="work-screen__title">Home Page</h3>
             <p className="work-screen__description">Página inicial, exibe um slideshow elegante que dá acesso a página em destaque do momento. No header tem os três pilares do site e o botão para acesso a área privada.</p>
@@ -83,7 +100,10 @@ function Section() {
       {/* Private Client Screens */}
       <div className="work-screen">
         <div className="container">
-          <img className="work-screen__image" src={privateClient} alt="Private Client" />
+          <picture>
+            <source srcSet={privateClientMin} media="(max-width: 480px)" />
+            <img className="work-screen__image" src={privateClient} alt="Private Client" />
+          </picture>
           <div className="work-screen__info">
             <h3 className="work-screen__title">Private Client</h3>
             <p className="work-screen__description">Página que expõe as principais vantagens de ser cliente do seguimento, e o leque de serviços oferecidos, como atendimento exclusivo e consultoria financeira.</p>
@@ -94,7 +114,10 @@ function Section() {
       {/* Investiments Screens */}
       <div className="work-screen">
         <div className="container">
-          <img className="work-screen__image" src={investiments} alt="Investiments" />
+          <picture>
+            <source srcSet={investimentsMin} media="(max-width: 480px)" />
+            <img className="work-screen__image" src={investiments} alt="Investiments" />
+          </picture>
           <div className="work-screen__info">
             <h3 className="work-screen__title">Investiments</h3>
             <p className="work-screen__description">Elenca nessa página as principais formas de investimentos de maneira clara, diversificada e direta.</p>
@@ -105,7 +128,10 @@ function Section() {
       {/* Financial Advisory Screens */}
       <div className="work-screen">
         <div className="container">
-          <img className="work-screen__image" src={financialAdvisory} alt="Financial Advisory" />
+          <picture>
+            <source srcSet={financialAdvisoryMin} media="(max-width: 480px)" />
+            <img className="work-screen__image" src={financialAdvisory} alt="Financial Advisory" />
+          </picture>
           <div className="work-screen__info">
             <h3 className="work-screen__title">Financial Advisory</h3>
             <p className="work-screen__description">Essa página detalha sobre a assessoria financira, discriminando produtos como cartões, seguros e tarifas de serviços.</p>
@@ -116,7 +142,10 @@ function Section() {
       {/* About The Client Screens */}
       <div className="work-screen">
         <div className="container">
-          <img className="work-screen__image" src={aboutTheClient} alt="About The Client" />
+          <picture>
+            <source srcSet={aboutTheClientMin} media="(max-width: 480px)" />
+            <img className="work-screen__image" src={aboutTheClient} alt="About The Client" />
+          </picture>
           <div className="work-screen__info">
             <h3 className="work-screen__title">About The Client</h3>
             <p className="work-screen__description">Página que detalha sobre o segmento e o próprio banco, de maneira segura, sólida e experiente.</p>
