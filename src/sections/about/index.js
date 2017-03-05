629d0d32511a0d2c75e111e5a7f6b9a7be42af1a
Ângelo Lucas <angelo.lrc@gmail.com>
2017-03-05 16:09:39 -0300

    header adjustments

--- src/sections/about/index.js
+++ src/sections/about/index.js
@@ -4,9 +4,12 @@ function About() {
   return (
     <div className="about-section">
       <div className="container">
-        <p>I’m 24, visual designer from Brasília, Brazil. <br /> Currently working at <a href="https://www.movamais.com/" target="_blank" rel="noopener noreferrer">Mova Mais.</a><br /> </p>
+        <p>I am 24, Brazilian born living in Brasília, Brazil.</p>
+        <p>Currently I am working as a Product Designer at <a href="https://www.movamais.com/" target="_blank" rel="noopener noreferrer">Mova Mais</a> <br />and before that I was working as a Designer at <a href="http://www.isobar.com/br/en/" target="_blank" rel="noopener noreferrer">Isobar Brasil.</a></p>
 
-        <p>I’m curious, self-taught and passionate about user experience, designing web and mobile to companies <a href="https://www.bb.com.br" target="_blank" rel="noopener noreferrer">Banco do Brasil</a>, <a href="http://www.embratur.gov.br/" target="_blank" rel="noopener noreferrer">Embratur</a>, <a href="http://www.cultura.gov.br/" target="_blank" rel="noopener noreferrer">Brazilian Ministry of Culture</a>, <a href="https://davidyurman.com" target="_blank" rel="noopener noreferrer">David Yurman</a>. My past experiences include <a href="http://www.isobar.com/br/en/" target="_blank" rel="noopener noreferrer">Isobar Brasil</a> and <a href="http://www.moringadigital.com.br/" target="_blank" rel="noopener noreferrer">Moringa Digital.</a></p>
+        <p>I am curious, self-taught, passionate about user experience <br />and I like to work with interfaces and finding opportunities.</p>
+
+        <p>A few brands I had the chance to work with are: <br /><a href="https://www.bb.com.br" target="_blank" rel="noopener noreferrer">Banco do Brasil</a>, <a href="http://www.embratur.gov.br/" target="_blank" rel="noopener noreferrer">Embratur</a>, <a href="https://www.caixa.gov.br" target="_blank" rel="noopener noreferrer">Caixa</a> and <a href="https://davidyurman.com" target="_blank" rel="noopener noreferrer">David Yurman</a>.</p>
       </div>
     </div>
   );
